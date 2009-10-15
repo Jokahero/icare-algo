c53752a2f193a845fee884b9543835bb9188055c
marc31boss <marc31boss@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-10-15 16:48:41 +0000

    Début de documentation de la classe Arbre Tests supplémentaires sur les expressions mathématiques

--- trunk/ExpressionsMathematiques/arbre.cpp
+++ trunk/ExpressionsMathematiques/arbre.cpp
@@ -1,33 +1,66 @@
 #include "arbre.h"
 
+/*! \brief Construit un arbre vide.
+
+  Initialise les sous-arbres à NULL, et une chaîne vide comme contenu.
+*/
 Arbre::Arbre() {
     m_sag = NULL;
     m_sad = NULL;
     m_contenu = QString::null;
 }
 
+/*! \brief Construit un arbre comportant des sous arbres.
+
+  \param pContenu Le contenu du noeud.
+  \param pSag Pointeur vers le sous-arbre gauche.
+  \param pSad Pointeur vers le sous-arbre droit.
+*/
 Arbre::Arbre(QString pContenu, Arbre* pSag, Arbre* pSad) {
     m_sag = pSag;
     m_sad = pSad;
     m_contenu = pContenu;
 }
 
+/*! \brief Retourne le sous-arbre gauche.
+
+  Si l'arbre est vide, retourne NULL.
+  \return Le sous-arbre gauche, ou NULL.
+*/
 Arbre* Arbre::getSag() {
     return m_sag;
 }
 
+/*! \brief Retourne le sous-arbre droit.
+
+  Si l'arbre est vide, retourne NULL.
+  \return Le sous-arbre droit, ou NULL.
+*/
 Arbre* Arbre::getSad() {
     return m_sad;
 }
 
+/*! \brief Retourne le contenu du noeud.
+
+  Si il n'y a pas de contenu, retourne une chaîne vide.
+  \return Le contenu du noeud.
+*/
 QString Arbre::getContenu() {
     return m_contenu;
 }
 
+/*! \brief Indique si l'arbre est vide.
+
+  \return Vrai si l'arbre est vide, faux sinon.
+*/
 bool Arbre::estFeuille() {
     return (m_sag == NULL);
 }
 
+/*! \brief Retourne la hauteur de l'arbre.
+
+  \return La hauteur de l'arbre.
+*/
 unsigned int Arbre::hauteur() {
     if (estFeuille())
         return 0;
@@ -37,18 +70,35 @@ unsigned int Arbre::hauteur() {
         return (getSad()->hauteur() + 1);
 }
 
+/*! \brief Définit le sous-arbre gauche de l'arbre.
+
+  \param pSag Le nouveau sous-arbre gauche.
+*/
 void Arbre::setSag(Arbre* pSag) {
     m_sag = pSag;
 }
 
+/*! \brief Définit le sous-arbre droit de l'arbre.
+
+  \param pSad Le nouveau sous-arbre droit.
+*/
 void Arbre::setSad(Arbre* pSad) {
     m_sad = pSad;
 }
 
+/*! \brief Définit le contenu de l'arbre.
+
+  \param pContenu Le nouveau contenu.
+*/
 void Arbre::setContenu(QString pContenu) {
     m_contenu = pContenu;
 }
 
+/*! \brief Affiche l'arbre.
+
+  Affiche récursivement le contenu et la hauteur de l'arbre, ainsi que de ses sous-arbres.
+  Fonction de développement, à supprimer dès qu'inutilisée.
+*/
 void Arbre::afficher() {
     qDebug() << "Hauteur : " << hauteur() << " : " << getContenu();
     if (!estFeuille()) {
--- trunk/ExpressionsMathematiques/tests/mathexp_tests.cpp
+++ trunk/ExpressionsMathematiques/tests/mathexp_tests.cpp
@@ -6,6 +6,7 @@ void MathExp_tests::testArbre() {
     Arbre* t1 = new Arbre();
     Arbre* t2 = new Arbre();
     Arbre* t3 = new Arbre("test", t1, t2);
+    // Tests des sous arbres
     QVERIFY(t1->estFeuille());
     QVERIFY(!t3->estFeuille());
     QCOMPARE(t3->getSag(), t1);
@@ -14,9 +15,11 @@ void MathExp_tests::testArbre() {
     t3->setSad(t1);
     QCOMPARE(t3->getSag(), t2);
     QCOMPARE(t3->getSad(), t1);
+    // Tests du contenu
     QCOMPARE(t3->getContenu(), QString("test"));
     t3->setContenu("toto");
     QCOMPARE(t3->getContenu(), QString("toto"));
+    // Tests de la hauteur
     QCOMPARE(t3->hauteur(), (unsigned int)1);
     QCOMPARE(t1->hauteur(), (unsigned int)0);
     t3->setSag(NULL);
@@ -30,11 +33,14 @@ void MathExp_tests::testArbre() {
 
 void MathExp_tests::testMathExp() {
     MathExp* m = new MathExp();
+    m->setExpression("3*5-2*2");
+    QCOMPARE(m->calcul(), (double)11);
     m->setExpression("5+(3-2)*5");
-    double c = m->calcul();
-    //Tree* t = m->getCalcul();
-    //t->afficher();
-    qDebug() << "Resultat : " << c;
+    QCOMPARE(m->calcul(), (double)10);
+    m->setExpression("5*(3-2)*5");
+    QCOMPARE(m->calcul(), (double)25);
+    m->setExpression("(1)");
+    QCOMPARE(m->calcul(), (double)1);
     delete m;
 }
 
