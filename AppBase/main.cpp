fe89e862e18d35dd5190146e74093795d9c544f9
marc31boss <marc31boss@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-10-20 20:28:49 +0000

    Ajout du support de QtLinguist

--- AppBase/main.cpp
+++ AppBase/main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QString locale = QLocale::system().name().section('_', 0, 0);
+
+    QTranslator translator;
+    translator.load(QString("icare_") + locale);
+    a.installTranslator(&translator);
+
+
     Window *fenetre = new Window();
     fenetre->show();
 
