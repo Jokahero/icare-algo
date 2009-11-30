385bf037c4448ced5bd4c54fd711b62fd2325082
clemence.ramondou <clemence.ramondou@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-11-30 20:47:42 +0000

    Analyse syntaxique : Ajout de la classe Instruction

--- Analyse/instruction.cpp
+++ Analyse/instruction.cpp
@@ -1,6 +1,27 @@
 #include "instruction.h"
 
-Instruction::Instruction(int pNumLigne, QString pLigne) {
+Instruction::Instruction(int pNumLigne, QString pLigne, QString pCategorie) {
     m_numLigne = pNumLigne;
     m_ligne = pLigne;
+    m_categorie = pCategorie;
 }
+
+int Instruction::getNumLigne(){
+    this->m_numLigne;
+}
+QString Instruction::getLigne(){
+    this->m_ligne;
+}
+QString Instruction::getCategorie(){
+    this->m_categorie;
+}
+void Instruction::setNumLigne(int pNumLigne){
+    this->m_numLigne = pNumLigne;
+}
+void Instruction::setLigne(QString pLigne){
+    this->m_ligne = pLigne;
+}
+void Instruction::setCategorie(QString pcategorie){
+    this->m_categorie = pcategorie;
+}
+
