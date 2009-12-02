fc4a3c3fc19ec6e5508b8acbd9c5cb96fd96d885
marc31boss <marc31boss@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-12-02 09:02:31 +0000

    Analyse Syntaxique : Début de la lecture des instructions

--- Analyse/analysesyntaxique.cpp
+++ Analyse/analysesyntaxique.cpp
@@ -8,6 +8,7 @@ AnalyseSyntaxique::AnalyseSyntaxique(Analyse* pAnalyse) {
 void AnalyseSyntaxique::lancer(QFile* pFichier) {
     qDebug() << "Analyse syntaxique commencée.";    //permet d'afficher un message
     lectureGlossaire(pFichier);
+    lectureInstructions(pFichier);
     emit terminee();
     qDebug() << "Analyse syntaxique terminée.";
 }
@@ -67,4 +68,37 @@ void AnalyseSyntaxique::lectureGlossaire(QFile* pFichier) {
     pFichier->close();
 
     qDebug() << "Lecture du glossaire terminée.";
+}
+
+void AnalyseSyntaxique::lectureInstructions(QFile* pFichier) {
+    qDebug() << "Lecture des instructions commencée.";
+
+    int debutAlgo = -1;
+    int finAlgo= -1;
+    QString ligneAct;
+    QRegExp rxDebut("^d[eé]but\\s*:?$");
+    QRegExp rxFin("^fin$");
+
+    rxDebut.setCaseSensitivity(Qt::CaseInsensitive);
+    rxFin.setCaseSensitivity(Qt::CaseInsensitive);
+
+    pFichier->open(QIODevice::ReadOnly | QIODevice::Text);
+
+    while (!pFichier->atEnd() && finAlgo < 0) {
+        ligneAct = pFichier->readLine().trimmed();
+        if (rxDebut.exactMatch(ligneAct))
+            debutAlgo = pFichier->pos();
+        else if (rxFin.exactMatch(ligneAct))
+            finAlgo = pFichier->pos();
+    }
+
+    pFichier->seek(debutAlgo);
+    while (pFichier->pos() < finAlgo) {
+        ligneAct = pFichier->readLine().trimmed();
+        // Si ce n'est pas un commentaire ou une ligne vide, l'ajouter à la liste d'instructions
+    }
+
+    pFichier->close();
+
+    qDebug() << "Lecture des instructions terminée.";
 }
