74cb9931d56d4fe10f5f86c11d10088ecac05c90
jokahero <jokahero@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-12-07 16:09:57 +0000

    Interface: modification de la fenêtre de préférences

--- trunk/Interface/preferences.h
+++ trunk/Interface/preferences.h
@@ -3,14 +3,36 @@
 
 #include <QtGui>
 
-class Preferences : public QWidget
-{
+class Preferences : public QDialog {
+
+    Q_OBJECT
+
 public:
     Preferences();
 
 private:
-    QLabel *commentairesLabel;
-    QPushButton *commentairesBouton;
+    QTabWidget *m_onglets;
+    QWidget *m_color;
+
+    QLabel *m_commentairesLabel;
+    QPushButton *m_commentairesBouton;
+
+    QLabel *m_bornesLabel;
+    QPushButton *m_bornesBouton;
+
+    QLabel *m_structuresLabel;
+    QPushButton *m_structuresBouton;
+
+    QLabel *m_numeriqueLabel;
+    QPushButton *m_numeriqueBouton;
+
+    QLabel *m_typeLabel;
+    QPushButton *m_typeBouton;
+
+    QPushButton *m_annuler;
+    QPushButton *m_quitter;
+public slots:
+    void modifierCouleur();
 };
 
 #endif // PREFERENCES_H
