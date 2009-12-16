6cd555f118d2645ab8232016e82da5bf6a4e78b8
lvvinx <lvvinx@9134bb38-b812-11de-8dfb-43c0dffa5306>
2009-12-16 16:16:56 +0000

    Modification du dictionnaire

--- Analyse/dictionnaire.h
+++ Analyse/dictionnaire.h
@@ -5,47 +5,69 @@
 
 class Dictionnaire {
 public:
+
+
     enum typeLigne {
+//Partie General
         Glossaire,
         DeclarationVariable,
         Debut,
         Fin,
         Commentaire,
-
-        FinPour,
-
-        Default,
-        FinSelon,
-
-        Repeter,
+//if...else
         Si,
         FinSi,
         Sinon,
+//switch
+        Selon,
+        Default,
+        Cas,
+        FinSelon,
+
+
+        FinPour,
+
+
+        Repeter,
+
+
 
         TypeInconnu,
     };
 
     static typeLigne getType(QString pLigne);
-
+//partie general
     static bool isGlossaire(QString pLigne);
     static bool isDeclarationVariable(QString pLigne);
     static bool isDebut(QString pLigne);
     static bool isFin(QString pLigne);
     static bool isCommentaire(QString pLigne);
-
-    static bool isFinPour(QString pLigne);
-
-    static bool isDefault(QString pLigne);
-    static bool isFinSelon(QString pLigne);
-
-    static bool isRepeter(QString pLigne);
-
+//If
     static bool isSi(QString pLigne);
     static bool isFinSi(QString pLigne);
     static bool isSinon(QString pLigne);
+//Switch
+    static bool isSelon(QString pLigne);
+    static bool isCas(QString pLigne);
+    static bool isDefault(QString pLigne);
+    static bool isFinSelon(QString pLigne);
+//For
+    static bool isFinPour(QString pLigne);
+
+    static bool isRepeter(QString pLigne);
+
+
+
+
 
 private:
-    QString listePipeVariable();
+
+    QString listePipeVariable(QStringList);
+
+
+
+
+
 };
 
 #endif // DICTIONNAIRE_H
