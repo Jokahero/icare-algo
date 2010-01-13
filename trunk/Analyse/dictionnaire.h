4d20df998bcbdc8b2eea392c7da60bd32698f6fb
lvvinx <lvvinx@9134bb38-b812-11de-8dfb-43c0dffa5306>
2010-01-13 17:12:05 +0000

    Mise à jour du dictionnaire : - Ajout de TantQue et Affectation. - Simplification du test d'expression régulière.

--- trunk/Analyse/dictionnaire.h
+++ trunk/Analyse/dictionnaire.h
@@ -8,27 +8,32 @@ public:
 
 
     enum typeLigne {
-        //Partie General
+//Partie General
         Glossaire,
         DeclarationVariable,
         Debut,
         Fin,
         Commentaire,
-        //if...else
+        Affectation,
+//if...else
         Si,
         FinSi,
         Sinon,
-        //switch
+//switch
         Selon,
         Default,
         Cas,
         FinSelon,
-
-
+//for
+        Pour,
         FinPour,
-
-
+//do..while
         Repeter,
+        Jusqua,
+//while
+        TanQue,
+        FinTantQue,
+
 
 
 
@@ -36,34 +41,33 @@ public:
     };
 
     static typeLigne getType(QString pLigne);
-    //partie general
+//partie general
     static bool isGlossaire(QString pLigne);
     static bool isDeclarationVariable(QString pLigne);
     static bool isDebut(QString pLigne);
     static bool isFin(QString pLigne);
     static bool isCommentaire(QString pLigne);
-    //If
+//If
     static bool isSi(QString pLigne);
     static bool isFinSi(QString pLigne);
     static bool isSinon(QString pLigne);
-    //Switch
+//Switch
     static bool isSelon(QString pLigne);
     static bool isCas(QString pLigne);
     static bool isDefault(QString pLigne);
     static bool isFinSelon(QString pLigne);
-    //For
-    static bool isPour(QString pLigne);
+//For
     static bool isFinPour(QString pLigne);
 
     static bool isRepeter(QString pLigne);
-    static bool isJusqua(QString pLigne);
+
 
 
 
 
 private:
 
-    static QString listePipeVariable();
+    static QString listePipeVariable(QStringList);
 
 
 
