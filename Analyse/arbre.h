7f2d4adab8f2a497c5f66e3983779cb1e37b6eda
marc31boss <marc31boss@9134bb38-b812-11de-8dfb-43c0dffa5306>
2010-02-18 15:34:26 +0000

    Analyse : Vérification des variables non initialisées sur le glossaire

--- Analyse/arbre.h
+++ Analyse/arbre.h
@@ -26,9 +26,9 @@ public:
     void setContenu(QString pContenu);
 
 private:
-    QString m_contenu;              /*!<\brief Expression contenue dans ce noeud de l'arbre. Elle peut être soit un opérateur si l'arbre n'est pas une feuille, soit une valeur numérique. */
-    Arbre* m_sag;                   /*!<\brief Pointeur vers le sous arbre gauche. */
-    Arbre* m_sad;                   /*!<\brief Pointeur vers le sous arbre droit. */
+    QString m_contenu;              /*! < \brief Expression contenue dans ce noeud de l'arbre. Elle peut être soit un opérateur si l'arbre n'est pas une feuille, soit une valeur numérique. */
+    Arbre* m_sag;                   /*! < \brief Pointeur vers le sous arbre gauche. */
+    Arbre* m_sad;                   /*! < \brief Pointeur vers le sous arbre droit. */
 };
 
 #endif // ARBRE_H
